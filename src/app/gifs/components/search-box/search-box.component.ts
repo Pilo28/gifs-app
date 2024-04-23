c52ee4ff5983cc31b5c076afd86e22d9a84924fe
Pablo <pablidefelitto@gmail.com>
2024-04-23 13:34:11 +0200

    added ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-box',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtTag')
+  public tagInput!:ElementRef<HTMLInputElement>
+
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value
+    console.log({newTag})
+  }
 }
   
